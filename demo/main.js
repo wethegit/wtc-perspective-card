65425b94cbfa3d7553248116c52984eb5a05c7ea
Liam Egan <liam@wethecollective.com>
2026-07-01 12:06:36 -0700

    Adding custom dialog

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,8 @@
 import { PerspectiveCard, ClickablePerspectiveCard } from '../src/index.js'
+import { CustomModalCard } from './custom-modal-card.js'
 import '../src/wtc-perspective-card.scss'
 import './demo.scss'
 
 document.querySelectorAll('.js-card').forEach((el) => new PerspectiveCard(el))
 document.querySelectorAll('.js-card--clickable').forEach((el) => new ClickablePerspectiveCard(el))
+document.querySelectorAll('.js-card--custom-modal').forEach((el) => new CustomModalCard(el))
